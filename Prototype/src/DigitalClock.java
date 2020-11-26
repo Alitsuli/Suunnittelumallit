cf53b3cebd91b40a35b01f64e5bbbfa12d7bb53e
altsuli <ali_reza222@hotmail.com>
2020-11-26 20:02:58 +0200

    Tehtävä 18

---
+++ Prototype/src/DigitalClock.java
@@ -0,0 +1,28 @@
+import java.util.Observable;
+import java.util.Observer;
+
+public class DigitalClock implements Observer {
+
+	private ClockTimer timer;
+
+	public DigitalClock(ClockTimer ct) {
+		timer = ct;
+		timer.addObserver(this);
+	}
+
+	public void update(Observable o, Object arg) {
+		if (o == timer) {
+			draw();
+		}
+	}
+
+	private void draw() {
+		int h = timer.getHour();
+		int m = timer.getMinute();
+		int s = timer.getSecond();
+
+		System.out.println("Digital Clock: " + h + "." + m + "." + s);
+
+	}
+
+}
---
+++ Prototype/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws InterruptedException {
+
+		ClockTimer timer = new ClockTimer();
+		boolean timerOn = true;
+
+		DigitalClock dc = new DigitalClock(timer);
+
+		while (timerOn == true) {
+
+			// 1 sec = 1000 ms
+			Thread.sleep(1000);
+			timer.tick();
+		}
+
+	}
+
+}
